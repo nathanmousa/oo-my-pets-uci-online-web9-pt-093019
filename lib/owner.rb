387e2ba4a65cb36a479c8fbc0bfc397dd10ed61e
Nathan Mousa <nathanmousa1@gmail.com>
2019-11-07 16:00:09 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,32 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    save
+  end
+  
+  def say_species
+    "I am a #{@species}."
+  end
+  
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  private
+  def save
+    @@all << self if !@@all.include?(self)
+  end
 end
