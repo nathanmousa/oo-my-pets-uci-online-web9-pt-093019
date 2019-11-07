e0c411d709997e9a7dd71d48173ddb04926004ae
Nathan Mousa <nathanmousa1@gmail.com>
2019-11-07 17:25:20 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,21 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  private
+  def save
+    @@all << self if !@@all.include?(self)
+  end
 end
